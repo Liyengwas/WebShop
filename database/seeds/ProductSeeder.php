59395a9603d277b4409858dad79838bd227946f2
liyengwas <liyengwas@gmail.com>
2020-05-06 23:07:50 +0300

    Basic Product Setup steps for the webshop

---
+++ database/seeds/ProductSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class ProductSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //Call Product Factory
+        factory(App\Product::class, 50)->create();
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+//Redirect Users to homepage
+Route::redirect('/', '/home');
