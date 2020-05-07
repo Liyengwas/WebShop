e3f36f20c12b4b3de31aed85bd135ca2a14e300f
liyengwas <liyengwas@gmail.com>
2020-05-07 14:00:30 +0300

    Products Page Adjustments

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,18 +7,20 @@
     <div class="row">
 
         @foreach ($AllProducts as $product)
-            <div class="card col-4">
+            <div class="col-4">
+                <div class="card" style="margin: 5px; padding: 5px;">
 
-                <img class="card-img-top" src="{{ asset('default-product-image.jpg') }}" alt="Card image cap" style="margin: 5px; padding: 3px;">
+                    <img class="card-img-top" src="{{ asset('default-product-image.jpg') }}" alt="Card image cap">
 
-                    <div class="card-body">
-                    <h4 class="card-title"> {{ $product->name }} </h4>
-                    <p class="card-text"> {{ $product->description }} </p>
-                    </div>
+                        <div class="card-body">
+                        <h4 class="card-title"> {{ $product->name }} </h4>
+                        <p class="card-text"> {{ $product->description }} </p>
+                        </div>
 
-                    <div class="card-footer">
-                        <a href="#" class="btn btn-outline-success card-link">Add to Cart</a>
-                    </div>
+                        <div class="card-footer">
+                            <a href="#" class="btn btn-outline-success card-link">Add to Cart</a>
+                        </div>
+                </div>
             </div>
         @endforeach
 
